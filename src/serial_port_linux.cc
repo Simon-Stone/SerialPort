41bddbf57fc89472061100d947f16f1e216945d6
nullpunktTUD <sipreuss@gmail.com>
2021-11-09 22:21:51 +0100

    Added basic Linux serial port

---
+++ src/serial_port_linux.cc
@@ -0,0 +1,53 @@
+#if defined(__linux__)
+
+#include <fcntl.h> // Contains file controls like O_RDWR
+#include <cerrno> // Error integer 
+#include <cstring> // strerror() function
+#include <termios.h> // Contains POSIX terminal control definitions
+#include <unistd.h> // write(), read(), close()
+
+#include "serial_port_linux.h"
+
+
+void serial_port::SerialPortLinux::Open()
+{
+	handle_ = open(settings_.port_name.c_str(), O_RDWR);
+
+	if (handle_ < 0)
+	{
+		printf("Error %i from open: %s\n", errno, std::strerror(errno));
+	}
+}
+
+void serial_port::SerialPortLinux::Close()
+{
+	close(handle_);
+	handle_ = -1;
+}
+
+bool serial_port::SerialPortLinux::IsOpen()
+{
+	return handle_ >= 0;
+}
+
+unsigned long serial_port::SerialPortLinux::NumBytesAvailable()
+{
+	return 0;
+}
+
+void serial_port::SerialPortLinux::FlushBuffer() const
+{
+}
+
+unsigned long serial_port::SerialPortLinux::ReadData(char* data, unsigned long num_bytes)
+{
+	return 0;
+}
+
+unsigned long serial_port::SerialPortLinux::WriteData(const char* data, unsigned long num_bytes)
+{
+	return 0;
+}
+
+
+#endif // __linux__
---
+++ src/serial_port_linux.h
@@ -0,0 +1,38 @@
+#ifndef SERIAL_PORT_LINUX_H_
+#define SERIAL_PORT_LINUX_H_
+
+#if defined(__linux__)
+
+#include <serial_port/serial_port.h>
+#include "interface.h"
+
+namespace serial_port
+{
+	class SerialPortLinux : public Interface
+	{
+	public:
+		// Inherit the constructors from the interface
+		using Interface::Interface;
+
+		// Make sure the port gets properly closed on destruction
+		~SerialPortLinux() override { SerialPortLinux::Close(); }
+
+		// Implement the interface
+		void Open() override;
+		void Close() override;
+		bool IsOpen() override;
+
+		unsigned long NumBytesAvailable() override;
+		void FlushBuffer() const override;
+
+		unsigned long ReadData(char* data, unsigned long num_bytes) override;
+		unsigned long WriteData(const char* data, unsigned long num_bytes) override;
+
+	private:
+		int handle_{ -1 };
+	};
+}
+
+#endif // __linux__
+
+#endif // !SERIAL_PORT_LINUX_H_
