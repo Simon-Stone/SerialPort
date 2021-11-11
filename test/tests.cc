e4018d21f9a61a63e8e4a84b80d4fa107181676a
Simon Stone <sipreuss@gmail.com>
2021-11-11 21:18:59 +0100

    Update tests.cc

--- test/tests.cc
+++ test/tests.cc
@@ -61,22 +61,20 @@ TEST(SerialPortTests, WriteString)
 // Test reading a C string
 TEST(SerialPortTests, ReadData)
 {
-	serial_port::SerialPort out_port(output_port_name, 115200);
-	serial_port::SerialPort in_port(input_port_name, 115200);
+	serial_port::SerialPort out_port(output_port_name, 9600);
+	serial_port::SerialPort in_port(input_port_name, 9600);
 
 	constexpr auto test_c_string = "I am a C string!\r\n";
 	const auto num_bytes = static_cast<unsigned long>(strlen(test_c_string));
 	out_port.Open();
 	in_port.Open();
 	// Get rid of possible lingering data from previous tests
-	out_port.FlushBuffer();
 	in_port.FlushBuffer();
-	
 
 	out_port.WriteData(test_c_string, num_bytes);
 
     // Allow some time for data transfer to take place
-    std::this_thread::sleep_for(std::chrono::milliseconds(20));
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
 
 	// Make Rx buffer one element larger to hold the terminating \0
 	char* buf = new char[num_bytes+1];
